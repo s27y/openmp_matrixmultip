ac25aed8ff8e14b8a0eb0ca7b5267d2d200fb9d7
Yang Sun <s27y@hotmail.com>
2014-11-01 18:18:38 +0000

    move logic to small functions

--- openmp_matrixmultip.c
+++ openmp_matrixmultip.c
@@ -3,6 +3,7 @@
 #include <omp.h>
 #include <cblas.h>
 #include <math.h>
+#include <sys/time.h>
 
 #define MAXTHRDS 124
 #define MAXMATRIXVALUE 100
@@ -17,93 +18,153 @@ int my_matrix_col_len;
 } matrix_multip_t;
 
 
-int main()
+/* array memorry allocating
+ * using omp static construct
+ */
+double** malloc_matrix(int matrix_size)
 {
-	double **A, **B, **C;
-	int num_of_thrds, matrix_size, i,j,k,x;
-	int submatrix_size;
-	matrix_size = 128;
-	time_t t;   
-   	/* Intializes random number generator */
-  	srand((unsigned) time(&t));
+	int i;
+  	double** m =(double **)malloc(matrix_size*sizeof(double *));
+  	m[0] = (double *)malloc(matrix_size*matrix_size*sizeof(double));
 
-	num_of_thrds = omp_get_num_procs();
-	omp_set_num_threads(num_of_thrds);
-
-	
-	submatrix_size = matrix_size/num_of_thrds;
-
-	//allocate and initialize arrays
-  	A =(double **)malloc(matrix_size*sizeof(double *));
-  	A[0] = (double *)malloc(matrix_size*matrix_size*sizeof(double));
-
-  	B =(double **)malloc(matrix_size*sizeof(double *));
-  	B[0] = (double *)malloc(matrix_size*matrix_size*sizeof(double));
-
-  	C =(double **)malloc(matrix_size*sizeof(double *));
-  	C[0] = (double *)malloc(matrix_size*matrix_size*sizeof(double));
-
-  	if (!A || !B || !C)
+  	if (!m)
     {
       printf( "Out of memory, reduce matrix_size value.\n");
       exit(-1);
     }
 
+    #pragma omp parallel
+    #pragma omp for schedule(static)
     for(i=1; i<matrix_size; i++)
     {
-      	A[i] = A[0]+i*matrix_size;
-      	if (!A[i])
+      	m[i] = m[0]+i*matrix_size;
+      	if (!m[i])
   		{
     		printf("memory failed \n");
     		exit(1);
   		}
     }
-    for(i=1; i<matrix_size; i++)
-    {
-      	B[i] = B[0]+i*matrix_size;
-      	if (!B[i])
-  		{
-    		printf("memory failed \n");
-    		exit(1);
-  		}
-    }
-    for(i=1; i<matrix_size; i++)
-    {
-      	C[i] = C[0]+i*matrix_size;
-      	if (!C[i])
-  		{
-    		printf("memory failed \n");
-    		exit(1);
-  		}
-    } 
+    return m;
+}
 
-  	// initialize the matrices
+/* nitialize arrays
+ * using omp dynamic construct
+ */
+void init_matrix(double** a, double** b, int matrix_size)
+{
+	int i,j;
+	time_t t;
+	/* Intializes random number generator */
+  	srand((unsigned) time(&t));
+
+  	#pragma omp parallel
+  	#pragma omp for schedule(dynamic)
    	for(i=0; i<matrix_size; i++)
    	{
     	for(j=0; j<matrix_size; j++)
     	{
-    		A[i][j] = i+j;
-    		B[i][j] = i+j+10;
+    		a[i][j] = i+j;
+    		b[i][j] = i+j+10;
    		 	// we can also create random matrices using the rand() method from the math.h library
     		//A[i][j] = rand() % MAXMATRIXVALUE;
     		//B[i][j] = rand() % MAXMATRIXVALUE;
 
-        	//printf("Value of A[%d][%d] %f\n",i,j,A[i][j]);
-        	//printf("Value of B[%d][%d] %f\n",i,j,B[i][j]);
+    		// comment out if need to check matrix value
+        	//printf("Value of A[%d][%d] %f\n",i,j,a[i][j]);
+        	//printf("Value of B[%d][%d] %f\n",i,j,b[i][j]);
     	}
     }
-	
+}
 
-	for(x=0; x<matrix_size; x++)
+void multiply_matrix(double** a, double** b, double** c, int matrix_size)
+{
+	int i;
+	for(i=0; i<matrix_size; i++)
     {
     	cblas_dgemm(CblasRowMajor,
                 CblasNoTrans, 
                 CblasNoTrans, 
                 1, matrix_size, matrix_size, 1.0,
-                *A + x*matrix_size, matrix_size, 
-                *B, matrix_size, 
-                1.0, *C + x*matrix_size, 
+                *a + i*matrix_size, matrix_size, 
+                *b, matrix_size, 
+                1.0, *c + i*matrix_size, 
                 matrix_size);
+    }
+}
+
+void multiply_matrix_omp_dynamic(double** a, double** b, double** c, int matrix_size)
+{
+	int i;
+	#pragma omp parallel
+  	#pragma omp for schedule(dynamic)
+	for(i=0; i<matrix_size; i++)
+    {
+    	cblas_dgemm(CblasRowMajor,
+                CblasNoTrans, 
+                CblasNoTrans, 
+                1, matrix_size, matrix_size, 1.0,
+                *a + i*matrix_size, matrix_size, 
+                *b, matrix_size, 
+                1.0, *c + i*matrix_size, 
+                matrix_size);
+    }
+}
+
+void multiply_matrix_omp_static(double** a, double** b, double** c, int matrix_size)
+{
+	int i;
+	#pragma omp parallel
+  	#pragma omp for schedule(static)
+	for(i=0; i<matrix_size; i++)
+    {
+    	cblas_dgemm(CblasRowMajor,
+                CblasNoTrans, 
+                CblasNoTrans, 
+                1, matrix_size, matrix_size, 1.0,
+                *a + i*matrix_size, matrix_size, 
+                *b, matrix_size, 
+                1.0, *c + i*matrix_size, 
+                matrix_size);
+    }
+}
+
+
+int main()
+{
+	struct timeval tv1, tv2;
+	struct timezone tz;
+	double **A, **B, **C;
+	int num_of_thrds, matrix_size, i,j,k,x;
+	int submatrix_size;
+	matrix_size = 128;
+
+	num_of_thrds = omp_get_num_procs();
+	omp_set_num_threads(num_of_thrds);
+
+	// if we want each thread process submatrix_size of matrix
+	// submatrix_size = matrix_size/num_of_thrds;
+
+	A = malloc_matrix(matrix_size);
+	B = malloc_matrix(matrix_size);
+	C = malloc_matrix(matrix_size);
+
+    init_matrix(A,B,matrix_size);	
+
+    gettimeofday(&tv1, &tz);
+    multiply_matrix_omp_dynamic(A,B,C,matrix_size);
+    gettimeofday(&tv2, &tz);
+
+    double elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+    printf("Multiply takes %lf sec \n", elapsed);
+
+
+
+    for(i=0; i<matrix_size; i++)
+   	{
+    	for(j=0; j<matrix_size; j++)
+    	{
+        	//printf("Value of C[%d][%d] %f\n",i,j,C[i][j]);
+    	}
     }
 
 
