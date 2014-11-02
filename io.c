6caa46fd6af8651b7c8dbfea875e9eb19a45c569
Yang Sun <s27y@hotmail.com>
2014-11-02 23:11:39 +0000

    finish up

---
+++ io.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void WriteData(char *fn, int n, double d)
+{
+   FILE *fp;
+   fp = fopen(fn,"a");
+   fprintf(fp, "%d,%lf\n", n, d);
+   fclose(fp);
+}
+
+
+void deleteOutputFile(char *fn)
+{
+   int status;
+   
+   status = remove(fn);
+ 
+   if( status == 0 )
+      printf("%s file deleted successfully.\n",fn);
+   else
+   {
+      printf("Unable to delete the file\n");
+      perror("Error");
+   }
+}
--- openmp_matrixmultip.c
+++ openmp_matrixmultip.c
@@ -4,9 +4,20 @@
 #include <cblas.h>
 #include <math.h>
 #include <sys/time.h>
+#include "io.c"
 
 #define MAXTHRDS 124
 #define MAXMATRIXVALUE 100
+#define OMP_STATIC_OUTPUT_FILENAME "out_omp_static.csv"
+#define OMP_DYNAMIC_OUTPUT_FILENAME "out_omp_dynamic.csv"
+#define SERIAL_OUTPUT_FILENAME "out_serial.csv"
+#define RESULT_CHECK_FILENAME "out_result"
+#define LOOP_START 4
+#define LOOP_END 11
+#define OMP_DYNAMIC "dynamic"
+#define OMP_STATIC "static"
+#define SERIAL "serial"
+
 
 typedef struct {
 int my_start_row;
@@ -20,6 +31,7 @@ int my_matrix_col_len;
 
 /* array memorry allocating
  * using omp static construct
+ *
  */
 double** malloc_matrix(int matrix_size)
 {
@@ -47,8 +59,9 @@ double** malloc_matrix(int matrix_size)
     return m;
 }
 
-/* nitialize arrays
+/* initialize arrays
  * using omp dynamic construct
+ *
  */
 void init_matrix(double** a, double** b, int matrix_size)
 {
@@ -76,6 +89,9 @@ void init_matrix(double** a, double** b, int matrix_size)
     }
 }
 
+/* multiply matrix using blas - serial
+ *
+ */
 void multiply_matrix(double** a, double** b, double** c, int matrix_size)
 {
 	int i;
@@ -92,6 +108,9 @@ void multiply_matrix(double** a, double** b, double** c, int matrix_size)
     }
 }
 
+/* multiply matrix using blas - dynamic
+ *
+ */
 void multiply_matrix_omp_dynamic(double** a, double** b, double** c, int matrix_size)
 {
 	int i;
@@ -110,6 +129,9 @@ void multiply_matrix_omp_dynamic(double** a, double** b, double** c, int matrix_
     }
 }
 
+/* multiply matrix using blas - static
+ *
+ */
 void multiply_matrix_omp_static(double** a, double** b, double** c, int matrix_size)
 {
 	int i;
@@ -128,47 +150,211 @@ void multiply_matrix_omp_static(double** a, double** b, double** c, int matrix_s
     }
 }
 
+/* calculate matrix one norm - omp
+ *
+ */
+int matrix_one_norm_omp(double** m,int matrix_size)
+{
+	int i,j;
 
-int main()
+	int max = 0;
+	int col_sum;
+	#pragma omp parallel for
+	#pragma omp shared(max)
+	#pragma omp private(col_sum)
+	for (i = 0; i < matrix_size; i++)
+	{
+		col_sum = 0;
+		#pragma omp parallel for reduction(+: col_sum)
+		for (j = 0; j < matrix_size; j++)
+		{
+			col_sum += abs(m[j][i]);
+		}
+		//printf("col_sum: %d\n", col_sum);
+		if (col_sum > max)
+		{
+			{
+				#pragma omp critical
+				max = col_sum;
+			}
+			
+		}
+	}
+	return max;
+}
+
+/* calculate matrix one norm - serial version
+ *
+ */
+int matrix_one_norm(double** m,int matrix_size)
+{
+	int i,j;
+
+	int max = 0;
+
+	for (i = 0; i < matrix_size; i++)
+	{
+		int col_sum = 0;
+		for (j = 0; j < matrix_size; j++)
+		{
+			col_sum += abs(m[j][i]);
+		}
+		//printf("col_sum: %d\n", col_sum);
+
+		if (col_sum > max)
+		{
+			max = col_sum;
+		}
+	}
+	return max;
+}
+
+
+int main(int argc, char *argv[])
 {
 	struct timeval tv1, tv2;
 	struct timezone tz;
 	double **A, **B, **C;
-	int num_of_thrds, matrix_size, i,j,k,x;
+	int numreps,num_of_thrds, matrix_size, i,j,k,x;
+    int one_norm_omp_dynamic, one_norm_omp_static, one_norm_serial;
 	int submatrix_size;
-	matrix_size = 128;
+    double average, elapsed;
+	matrix_size = 1024;
 
 	num_of_thrds = omp_get_num_procs();
 	omp_set_num_threads(num_of_thrds);
+	printf("Number of thread: %d\n", num_of_thrds);
+    numreps = 3;
 
 	// if we want each thread process submatrix_size of matrix
 	// submatrix_size = matrix_size/num_of_thrds;
 
-	A = malloc_matrix(matrix_size);
-	B = malloc_matrix(matrix_size);
-	C = malloc_matrix(matrix_size);
 
-    init_matrix(A,B,matrix_size);	
+    if( argc == 2 )
+   {
+      printf("The argument supplied is '%s'\n", argv[1]);
+      if(strcmp(argv[1],OMP_DYNAMIC) != 0 || strcmp(argv[1],OMP_STATIC) != 0 || strcmp(argv[1],SERIAL) != 0)
+      {
+        printf("The argument supplied is not  %s, %s or %s \n", OMP_DYNAMIC, OMP_STATIC, SERIAL);
+      }
+   }
+   else if( argc > 2 )
+   {
+      printf("Too many arguments supplied.\n");
+   }
+   else
+   {
+      printf("One argument expected.\n");
+   }
 
-    gettimeofday(&tv1, &tz);
-    multiply_matrix_omp_dynamic(A,B,C,matrix_size);
-    gettimeofday(&tv2, &tz);
 
-    double elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
-    printf("Multiply takes %lf sec \n", elapsed);
+    if(strcmp(argv[1],OMP_DYNAMIC) == 0)
+    {
 
+    deleteOutputFile(OMP_STATIC_OUTPUT_FILENAME);
+    for(i = LOOP_START; i < LOOP_END; i++)
+    {
+        matrix_size = pow(2, i);
 
+        A = malloc_matrix(matrix_size);
+        B = malloc_matrix(matrix_size);
+        C = malloc_matrix(matrix_size);
+        init_matrix(A,B,matrix_size);
 
-    for(i=0; i<matrix_size; i++)
-   	{
-    	for(j=0; j<matrix_size; j++)
-    	{
-        	//printf("Value of C[%d][%d] %f\n",i,j,C[i][j]);
-    	}
+        for(j = 0; j < numreps; j++)
+        {
+            // omp dynamic
+            gettimeofday(&tv1, &tz);
+            multiply_matrix_omp_static(A,B,C,matrix_size);
+            one_norm_omp_static = matrix_one_norm_omp(C, matrix_size);
+            gettimeofday(&tv2, &tz);
+            elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+            average += elapsed;
+            printf("matrix size %d, matrix_one_norm_omp takes %lf sec \n", matrix_size, elapsed);
+
+        }
+        average= average/3;
+
+        WriteData(OMP_STATIC_OUTPUT_FILENAME, matrix_size, average);
+       
+    }
+    WriteData(RESULT_CHECK_FILENAME, 1, one_norm_omp_static);
+
+    free(A);
+    free(B);
+    free(C);
+    elapsed = 0;
+    average =0;
+
     }
 
+    if(strcmp(argv[1],OMP_STATIC) == 0)
+    {
+    deleteOutputFile(OMP_DYNAMIC_OUTPUT_FILENAME);
+    for(i = LOOP_START; i < LOOP_END; i++)
+    {
+        matrix_size = pow(2, i);
 
-free(A);
-free(B);
-free(C);
+        A = malloc_matrix(matrix_size);
+        B = malloc_matrix(matrix_size);
+        C = malloc_matrix(matrix_size);
+        init_matrix(A,B,matrix_size);
+
+        for(j = 0; j < numreps; j++)
+        {
+            // omp dynamic
+            gettimeofday(&tv1, &tz);
+            multiply_matrix_omp_dynamic(A,B,C,matrix_size);
+            one_norm_omp_dynamic = matrix_one_norm_omp(C, matrix_size);
+            gettimeofday(&tv2, &tz);
+            
+            elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+            average += elapsed;
+            printf("matrix size %d, matrix_one_norm_omp takes %lf sec \n", matrix_size, elapsed);
+        }
+        average= average/3;
+        WriteData(OMP_DYNAMIC_OUTPUT_FILENAME, matrix_size, average);
+    }
+    WriteData(RESULT_CHECK_FILENAME, 2, one_norm_omp_dynamic);
+    free(A);
+    free(B);
+    free(C);
+    elapsed = 0;
+    average = 0;
+    }
+
+    if(strcmp(argv[1],SERIAL) == 0)
+    {
+    deleteOutputFile(SERIAL_OUTPUT_FILENAME);
+    for(i = LOOP_START; i < LOOP_END; i++)
+    {
+        matrix_size = pow(2, i);
+
+        A = malloc_matrix(matrix_size);
+        B = malloc_matrix(matrix_size);
+        C = malloc_matrix(matrix_size);
+        init_matrix(A,B,matrix_size);
+
+        for(j = 0; j < numreps; j++)
+        {
+        // omp dynamic
+        gettimeofday(&tv1, &tz);
+        multiply_matrix(A,B,C,matrix_size);
+        one_norm_serial = matrix_one_norm(C, matrix_size);
+        gettimeofday(&tv2, &tz);
+
+        elapsed = (double) (tv2.tv_sec-tv1.tv_sec) + (double) (tv2.tv_usec-tv1.tv_usec) * 1.e-6;
+        average += elapsed;
+        printf("matrix size %d, matrix_one_norm_omp static takes %lf sec \n", matrix_size, elapsed);
+
+        }
+        average = average/3;
+        WriteData(SERIAL_OUTPUT_FILENAME, matrix_size, average);
+    }
+    WriteData(RESULT_CHECK_FILENAME, 3, one_norm_serial);
+    free(A);
+    free(B);
+    free(C);
+    }
+
 }
